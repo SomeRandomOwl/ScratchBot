e3c29673e12986e0335a04513f6a861128658960
Ethan Ary <ethanary@gmail.com>
2016-05-09 14:57:15 -0500

    accidatal caps

--- index.js
+++ index.js
@@ -1040,7 +1040,7 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
             messageSend(channelID, "Check your PM's :mailbox_with_mail:")
             for (var i = 0; i < doc.cList.length; i++) {
                 if (i !== doc.cList.length) {
-                    cList = Clist + doc.cList[i] + " , "
+                    cList = clist + doc.cList[i] + " , "
                 }
                 else {
                     cList = cList + doc.cList[i]
