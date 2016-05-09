5658100cfafe23694c8e8f989b9aa0763bf56bed
Ethan Ary <ethanary@gmail.com>
2016-05-09 12:36:58 -0500

    log to server folders

--- index.js
+++ index.js
@@ -1189,13 +1189,19 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
         timed = timed.replace('GMT-0500 (Central Daylight Time)', '')
         if (channelID in bot.directMessages) {
             console.log(timed + 'Channel: ' + 'PM | ' + user + ': ' + message)
-            fs.appendFile("logs/" + user + ".txt", '\n' + timed + user + ": " + message)
+            fs.appendFile("logs/DMs" + user + ".txt", '\n' + timed + user + ": " + message)
         } else {
             servern = bot.servers[serverID].name
             channeln = bot.servers[serverID].channels[channelID].name
+            try {
+                stats = fs.lstatSync('./logs/' + servern);
+            } catch (e) {
+                mkdirp('./logs' + servern, function(err) {
+                    console.log(err)
+                });
+            }
             console.log(timed + 'Channel: ' + servern + '/' + channeln + ' | ' + user + ': ' + message)
-            //fs.appendFile("logs/Main LOG.txt", '\n' + timed + user + ": " + message)
-            fs.appendFile("logs/" + servern + '.' + channeln + '.txt', '\n' + timed + user + ": " + message)
+            fs.appendFile("logs/" + servern + '/' + channeln + '.txt', '\n' + timed + user + ": " + message)
         }
     } else if (userID.indexOf('104867073343127552') != 0 || channelID.indexOf('164845697508704257') != 0 && rconcmd === "Yes" && ignore !== true) {
         if (ignore !== true) {
