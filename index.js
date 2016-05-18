321cde448f6e5192d5e23f1215b346412b9d1b24
Ethan Ary <ethanary@gmail.com>
2016-05-18 17:23:28 -0500

    info

--- index.js
+++ index.js
@@ -1285,7 +1285,10 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
             }
         }
         if (message.indexOf(commandmod) === 0 && message.toLowerCase().indexOf('info') !== -1 && ignore !== true) {
-            messageSend(userID, doc.info)
+            messageSend(userID, "Currently connected to: " + serverCnt + " Servers\n" +
+                "With a total of: " + channelCnt + " Channels\n" +
+                "With approximatly: " + userCnt + " Users across all of them\n" +
+                doc.info)
         }
         if (message.indexOf(commandmod) === 0 && message.toLowerCase().indexOf('roll') !== -1 && ignore !== true) {
             var msg = message
