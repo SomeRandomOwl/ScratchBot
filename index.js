7ea31cffffde504420943de33c12f5900f084e61
Ethan Ary <ethanary@gmail.com>
2016-05-19 01:09:11 -0500

    ok?

--- index.js
+++ index.js
@@ -1615,7 +1615,7 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
                 fs.appendFile("./logs/" + servern + '/' + channeln + '.txt', '\n' + timed + user + ": " + message)
             })
             try {
-                console.log(verb)
+                //cconsole.log(verb)
                 console.log(storage.d.Servers.settings[sname].verb)
                 if (verb || cnaid === channelID || storage.d.Servers.settings[sname].verb) {
                     console.log(timed + 'Channel: ' + servern + '/' + channeln + ' | ' + user + ': ' + message)
