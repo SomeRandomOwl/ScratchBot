f1cbc82a5f7012cc399d20b046fbe8783cc51382
Ethan Ary <ethanary@gmail.com>
2016-05-24 11:19:23 -0500

    sentance case

--- index.js
+++ index.js
@@ -1034,6 +1034,21 @@ function toTitleCase(str) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
+/*/Sentance Case/*/
+function toSentanceCase(string) {
+    var n = string.split(".");
+    var vfinal = ""
+    for (i = 0; i < n.length; i++) {
+        var spaceput = ""
+        var spaceCount = n[i].replace(/^(\s*).*$/, "$1").length;
+        n[i] = n[i].replace(/^\s+/, "");
+        var newstring = n[i].charAt(n[i]).toUpperCase() + n[i].slice(1);
+        for (j = 0; j < spaceCount; j++)
+            spaceput = spaceput + " ";
+        vfinal = vfinal + spaceput + newstring + ".";
+    }
+    vfinal = vfinal.substring(0, vfinal.length - 1);
+}
 /*/Word!/*/
 function wordNik(cl, channelID, userID, word, type, debug) {
     if (type === 'def') {
