889eaa7a7efd171f83ce3b364a80c374d8fa8253
Ethan Ary <ethanary@gmail.com>
2016-05-27 10:46:11 -0500

    timeat

--- index.js
+++ index.js
@@ -1364,20 +1364,6 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
         console.log(e)
     }*/
     //Logging Related
-    /*var re = new RegExp("^([a-z0-9]{20,})$")
-    if (re.test(message)) {
-        console.log('yes')
-        bot.deleteMessage({
-            channel: channelID,
-            messageID: messageID
-        }, function(error) {
-            if (error) {
-                console.log('No Permission to delete the spam')
-            } else {
-                messageSend(channelID, "No spam messages please! ", false, null, true, userID)
-            }
-        });
-    }*/
     if (storage.d.Users[user] !== undefined) {
         if (storage.d.Users[user].messageCnt === undefined) {
             storage.d.Users[user].messageCnt = 1
@@ -1389,12 +1375,12 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
         writeJSON('./assets/storage', storage)
     }
     if (message.toLowerCase().indexOf('http') !== -1) {
-        var timeAt = moment().format('[MMMM Do YYYY, HH:mm:ss]')
+        var timeAt = moment().format('[MMMM Do YYYY, HH:mm:ss] ')
         logger.info(chalk.gray("Link Posted, logging to file"))
         if (message.indexOf(' ', message.indexOf('http')) === -1) {
-            var link = user + ': ' + message.substring(message.indexOf('http'))
+            var link = timeAt + user + ': ' + message.substring(message.indexOf('http'))
         } else if (message.indexOf(' ', message.indexOf('http')) !== -1) {
-            var link = user + ': ' + message.substring(message.indexOf('http'), message.indexOf(' ', message.indexOf('http')))
+            var link = timeAt + user + ': ' + message.substring(message.indexOf('http'), message.indexOf(' ', message.indexOf('http')))
         }
         if (storage.d.Users[user] !== undefined) {
             if (storage.d.Users[user].linkCnt === undefined) {
