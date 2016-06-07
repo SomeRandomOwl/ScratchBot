c936d376cd439c2931ba4c12cd983213a68e752c
Ethan Ary <ethanary@gmail.com>
2016-06-07 13:27:09 -0500

    s

--- index.js
+++ index.js
@@ -958,9 +958,9 @@ function whoIs(channelID, serverID, name) {
         rolesm = 'everyone, '
         for (var i = 0; i < roles.length; i++) {
             if (i !== roles.length - 1) {
-                rolesm = rolesm + bot.server[serverID].roles[roles[i]] + ', '
+                rolesm = rolesm + bot.servers[serverID].roles[roles[i]] + ', '
             } else {
-                rolesm = rolesm + bot.server[serverID].roles[roles[i]]
+                rolesm = rolesm + bot.servers[serverID].roles[roles[i]]
             }
         }
     } else {
