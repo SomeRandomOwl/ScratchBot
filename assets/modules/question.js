40ed6c03f6fe02a50c6782649e9c89bf833bd2de
Ethan Ary <ethanary@gmail.com>
2016-06-28 18:17:44 -0500

    redundantcy

--- assets/modules/question.js
+++ assets/modules/question.js
@@ -3,6 +3,7 @@ var shortid = require('shortid');
 
 exports.ask = function(question) {
     questions[shortid.generate()] = question
+    questions = JSON.parse(JSON.stringify(questions, null, '\t'))
     console.log(questions)
 }
 exports.answer = function(answer) {
