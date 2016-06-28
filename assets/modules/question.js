7598ce0840f512b518e9099b06d309c352e421bb
Ethan Ary <ethanary@gmail.com>
2016-06-28 18:19:39 -0500

    ok

--- assets/modules/question.js
+++ assets/modules/question.js
@@ -2,9 +2,11 @@ var questions = {}
 var shortid = require('shortid');
 
 exports.ask = function(question) {
-    questions[shortid.generate()] = question
+    id = shortid.generate()
+    questions[id] = question
     questions = JSON.parse(JSON.stringify(questions, null, '\t'))
     console.log(questions)
+    return id
 }
 exports.answer = function(answer) {
     console.log('yes')
