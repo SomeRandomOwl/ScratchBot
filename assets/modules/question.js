cb66d88be9a1b6bc629cc4845c949e3c228611fd
Ethan Ary <ethanary@gmail.com>
2016-06-28 18:12:10 -0500

    revert

--- assets/modules/question.js
+++ assets/modules/question.js
@@ -1,16 +1,11 @@
-var activeQ = {}
+var questions = {}
 var shortid = require('shortid');
 
-var ask = function(question) {
+exports.ask = function(question) {
     questions[shortid.generate()] = question
     console.log(questions)
 }
-var answer = function(answer) {
+exports.answer = function(answer) {
     console.log('yes')
 }
-var exp = {
-    activeQ,
-    ask,
-    answer
-}
-module.exports = exp
+exports.active = questions
