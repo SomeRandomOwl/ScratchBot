16980488d481922d6a7e9927c90212eacb4e04c4
Ethan Ary <ethanary@gmail.com>
2016-07-07 16:37:48 -0500

    ok?

--- assets/modules/voiceLines.js
+++ assets/modules/voiceLines.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var vlJ = require('../voiceLines/voiceL.json')
-var cmds = require('./')
+var cmds = require('./assets/modules')
 var util = require('util'),
     exec = require('child_process').exec,
     child
