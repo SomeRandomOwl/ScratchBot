d534ba833b56b0fe796a183ae1a89390c077e8dd
Ethan Ary <ethanary@gmail.com>
2016-07-12 21:51:13 -0500

    ok

--- assets/modules/index.js
+++ assets/modules/index.js
@@ -1,31 +1,22 @@
-module.exports = function(bot, storage, config) {
-    const module = {};
-
-    console.log(bot)
-
-    var secondsToTime = require('./secondsToTime.js')
-    var gettime = require('./gettime.js')
-    var writeJSON = require('./writeJSON.js')
-    var isInArray = require('./isInArray.js')
-    var whoIs = require('./whoIs.js')
-    var toSentenceCase = require('./toSentenceCase.js')
-        //var messageSend = require('./messageSend.js')(bot, storage)
-
-    var util = {
-        secondsToTime,
-        gettime,
-        writeJSON,
-        isInArray,
-        whoIs,
-        toSentenceCase,
-        //messageSend
-    }
-
-    module.voiceLines = require('./voiceLines.js')(util)
-    module.mtg = require('./mtg.js')(util)
-    module.list = require('./lists.js')(util)
-    module.question = require('./question.js')(util)
-    module.util = util
-
-    return module;
+var secondsToTime = require('./secondsToTime.js')
+var gettime = require('./gettime.js')
+var writeJSON = require('./writeJSON.js')
+var isInArray = require('./isInArray.js')
+var whoIs = require('./whoIs.js')
+var toSentenceCase = require('./toSentenceCase.js')
+    //var messageSend = require('./messageSend.js')(bot, storage)
+var util = {
+    secondsToTime,
+    gettime,
+    writeJSON,
+    isInArray,
+    whoIs,
+    toSentenceCase
+    //messageSend
 }
+
+exports.voiceLines = require('./voiceLines.js')
+exports.mtg = require('./mtg.js')
+exports.list = require('./lists.js')
+exports.question = require('./question.js')
+exports.util = util
