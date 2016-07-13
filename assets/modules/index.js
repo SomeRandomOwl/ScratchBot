eee5b0181d2349f152ee473ce7cb98c1093ff902
Ethan Ary <ethanary@gmail.com>
2016-07-12 21:16:20 -0500

    wook forgot something!

--- assets/modules/index.js
+++ assets/modules/index.js
@@ -1,23 +1,30 @@
-var secondsToTime = require('./secondsToTime.js')
-var gettime = require('./gettime.js')
-var writeJSON = require('./writeJSON.js')
-var isInArray = require('./isInArray.js')
-var whoIs = require('./whoIs.js')
-var toSentenceCase = require('./toSentenceCase.js')
-    //var messageSend = require('./messageSend.js')(bot, storage)
+module.exports = function(bot, storage, config) {
+    const module = {};
 
-var util = {
-    secondsToTime,
-    gettime,
-    writeJSON,
-    isInArray,
-    whoIs,
-    toSentenceCase
-    //messageSend
+
+    var secondsToTime = require('./secondsToTime.js')
+    var gettime = require('./gettime.js')
+    var writeJSON = require('./writeJSON.js')
+    var isInArray = require('./isInArray.js')
+    var whoIs = require('./whoIs.js')
+    var toSentenceCase = require('./toSentenceCase.js')
+        //var messageSend = require('./messageSend.js')(bot, storage)
+
+    var util = {
+        secondsToTime,
+        gettime,
+        writeJSON,
+        isInArray,
+        whoIs,
+        toSentenceCase
+        //messageSend
+    }
+
+    module.voiceLines = require('./voiceLines.js')
+    module.mtg = require('./mtg.js')
+    module.list = require('./lists.js')
+    module.question = require('./question.js')
+    module.util = util
+
+    return module;
 }
-
-exports.voiceLines = require('./voiceLines.js')
-exports.mtg = require('./mtg.js')
-exports.list = require('./lists.js')
-exports.question = require('./question.js')
-exports.util = util
