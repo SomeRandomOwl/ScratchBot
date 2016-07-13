eb5a6e41c0d3b6d96bbc2b736636e9a6b8e4d57f
Ethan Ary <ethanary@gmail.com>
2016-07-12 20:58:46 -0500

    ok!

--- assets/modules/index.js
+++ assets/modules/index.js
@@ -5,7 +5,7 @@ var isInArray = require('./isInArray.js')
 var whoIs = require('./whoIs.js')
 var toSentenceCase = require('./toSentenceCase.js')
     //var messageSend = require('./messageSend.js')(bot, storage)
-console.log(bot)
+o
 var util = {
     secondsToTime,
     gettime,
