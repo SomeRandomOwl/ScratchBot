a14b84d45005e5e5cd5dd47b83bc26c431e0a052
Ethan Ary <ethanary@gmail.com>
2016-07-21 14:07:32 -0500

    Creation Date.js

---
+++ assets/modules/creationDate.js
@@ -0,0 +1,10 @@
+module.exports = function(userID, callback) {
+    const timestamp = new Date(parseInt('141694315251367936') / 4194304 + 1420070400000);
+    const createdM = moment.utc(timestamp);
+    const created = createdM.format("MM-DD-YYYY hh:mm:ss");
+    if (typeof callback === "function") {
+        const err = false;
+        const response = created;
+        callback(err, response);
+    }
+}
