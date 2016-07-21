9d84ceef328f4c344693c9dbae02362484ce050c
Ethan Ary <ethanary@gmail.com>
2016-07-21 15:51:12 -0500

    fixed

--- assets/modules/index.js
+++ assets/modules/index.js
@@ -20,5 +20,4 @@ exports.mtg = require('./mtg.js')
 exports.list = require('./lists.js')
 exports.question = require('./question.js')
 exports.creationDate = require('./creationDate.js')
-exports.gNG = require('./gNG.js')
 exports.util = util
