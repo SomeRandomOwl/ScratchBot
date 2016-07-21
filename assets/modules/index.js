c98974bf75039d5458df6743b8a6cb49c0855a66
Ethan Ary <ethanary@gmail.com>
2016-07-21 15:14:08 -0500

    test?

--- assets/modules/index.js
+++ assets/modules/index.js
@@ -20,4 +20,5 @@ exports.mtg = require('./mtg.js')
 exports.list = require('./lists.js')
 exports.question = require('./question.js')
 exports.creationDate = require('./creationDate.js')
+exports.generateStatus = require('./generateStatus.js')
 exports.util = util
