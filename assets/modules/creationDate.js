43a002589329bce606c7e2c6103474a4afd2a5bc
Ethan Ary <ethanary@gmail.com>
2016-07-22 23:52:15 -0500

    ID

--- assets/modules/creationDate.js
+++ assets/modules/creationDate.js
@@ -1,6 +1,6 @@
 var moment = require('moment');
-module.exports = function(userID, callback) {
-    const timestamp = new Date(parseInt('141694315251367936') / 4194304 + 1420070400000);
+module.exports = function(ID, callback) {
+    const timestamp = new Date(parseInt(ID) / 4194304 + 1420070400000);
     const createdM = moment.utc(timestamp);
     const created = createdM.format("MM-DD-YYYY hh:mm:ss");
     if (typeof callback === "function") {
