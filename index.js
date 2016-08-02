a6a5654011c11adac372a35249f9bf00b0c49d5c
Ethan Ary <ethanary@gmail.com>
2016-08-02 18:44:23 -0500

    discord

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Welcome, this is scratch bots source code, everything that makes her run and tick! */
-var DiscordClient = require('discord.io');
+var Discord = require('discord.io');
 var winston = require('winston');
 var config = require('../../config.json');
 var fs = require('fs');
