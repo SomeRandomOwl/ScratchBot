71f604ae06786ed65e1d1eff51556250d7f21a38
Ethan Ary <ethanary@gmail.com>
2016-08-04 17:23:26 -0500

    forgot a ,

--- index.js
+++ index.js
@@ -1013,7 +1013,7 @@ var query = {
                     })
                 }
             })
-        }
+        },
         'i': function(channelID, query) {
             queryC = query.substring(0, query.indexOf('|'))
             queryV = query.substring(query.indexOf('|') + 1)
