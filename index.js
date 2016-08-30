7a1589ad95a128b46f22113b5ea31ff70ce0673b
Ethan Ary <ethanary@gmail.com>
2016-08-30 12:36:23 -0500

    ?

--- index.js
+++ index.js
@@ -1579,19 +1579,47 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
     }
     if (sname !== undefined) {
         db.clq({
-            type: 'update',
+            type: 'select',
+            what: 'serverid, messageCnt',
             location: 'servers',
             id: 'serverid',
-            where: serverID,
-            change: [
-                ['messageCnt'],
-                [
-                    res[0].messageCnt + 1
-                ]
-            ]
-        }, function(e, r) {
-            if (e !== null) {
-                console.log(e)
+            where: serverID
+        }, function(err, res) {
+            try {
+                if (res[0] === undefined) {
+                    db.clq({
+                        type: 'insert',
+                        location: 'channels',
+                        change: [
+                            ['serverid', 'name', 'messageCnt', 'sOwnerId'],
+                            [
+                                serverID,
+                                sname,
+                                '1',
+                                bot.servers[serverID].owner_id
+                            ]
+                        ]
+                    })
+                } else {
+                    db.clq({
+                        type: 'update',
+                        location: 'servers',
+                        id: 'serverid',
+                        where: serverID,
+                        change: [
+                            ['messageCnt'],
+                            [
+                                res[0].messageCnt + 1
+                            ]
+                        ]
+                    }, function(e, r) {
+                        if (e !== null) {
+                            console.log(e)
+                        }
+                    })
+                }
+            } catch (E) {
+                console.log(E)
             }
         })
     }
